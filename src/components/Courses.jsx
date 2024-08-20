d352bffa9baaa36048345034e5382dadfba47440
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-08-20 23:34:42 +0530

    Courses jsx file added.

---
+++ src/components/Courses.jsx
@@ -0,0 +1,17 @@
+const Courses = () => {
+	return (
+		<div className="flex flex-col gap-2.5">
+			<div className="w-full">
+				<h1 className="text-[22px] font-semibold ">COURSES</h1>
+				<div className="border-2 border-black"></div>
+			</div>
+			<div>
+				<h1 className="text-[16] font-semibold">
+					Full Stack JavaScript Web Developer Bootcamp - iNeuron
+				</h1>
+			</div>
+		</div>
+	);
+};
+
+export default Courses;
