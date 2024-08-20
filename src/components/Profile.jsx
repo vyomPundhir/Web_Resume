cafce1f9e6721d653883aab08c5c53b5a88ba9ba
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-08-20 23:36:09 +0530

    Profile jsx file added.

---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+const Profile = () => {
+	return (
+		<div className="flex flex-col gap-2.5">
+			<div className="w-full">
+				<h1 className="text-[20px] font-semibold ">PROFILE</h1>
+				<div className="border-2 border-black "></div>
+			</div>
+			<div className="mt-0 text-[13.5px]">
+			I am a highly motivated individual with a passion of technology and a strong desire to learn. I am excited to begin my career as a Frontend JavaScript Developer and make a meaningful impact in the field.
+			I am eager to take on the job of a Frontend JavaScript Developer. I am a quick learner, with a strong foundation in problem solving and critical thinking.
+			I am confident in my ability to quickly adapt to new technologies and programming concepts.
+			I am excited to begin my career as a Frontend JavaScript Developer and make a meaningful impact in the field.
+			</div>
+		</div>
+	);
+};
+
+export default Profile;
